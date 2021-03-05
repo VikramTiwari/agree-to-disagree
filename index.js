30a2da19b61e7bf9e5ab35c0e293b9c21af477b1
Vikram Tiwari <vikramtheone1@gmail.com>
2021-03-04 22:56:44 -0800

    fix: use print media and save data as string too

--- index.js
+++ index.js
@@ -1,25 +1,31 @@
 const path = require('path')
 const { chromium } = require('playwright')
+const { writeFileSync } = require('fs')
+
 const { addCommitAndPush } = require('./repo')
 
 const indexes = require('./indexes.json')
 
-async function getScreenshot(browser, index) {
-	const page = await browser.newPage()
+async function getScreenshot(page, index) {
 	await page.goto(index.url)
+	await page.waitForLoadState(`domcontentloaded`)
+	const text = await page.innerText(`body`)
 	await page.screenshot({
 		path: path.join(__dirname, `collection/${index.id}.png`),
 		fullPage: true,
 	})
+	writeFileSync(path.join(__dirname, `collection/${index.id}.txt`), text, { encoding: `utf-8` })
 }
 
 async function start() {
 	const browser = await chromium.launch()
+	const page = await browser.newPage()
+	await page.emulateMedia({ media: 'print' })
 	const promises = []
 	for (let i = 0; i < indexes.length; i++) {
 		const index = indexes[i]
 		console.log(`adding ${index.id} from ${index.url}`)
-		await getScreenshot(browser, index)
+		await getScreenshot(page, index)
 		console.log(`added ${index.id} from ${index.url}`)
 	}
 	await Promise.allSettled(promises)
